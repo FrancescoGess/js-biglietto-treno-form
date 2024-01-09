12433f9b78ff5ba3d1a0e53162dfaac26bb067cf
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-01-09 21:37:54 +0100

    exercise complete

--- main.js
+++ main.js
@@ -16,9 +16,57 @@
 //Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 //definire le constanti
-const nomePasseggero = document.getElementById('name');
-const kmPercorso = document.getElementById('km');
-const etaPasseggero = document.getElementById('eta');
-const sendButton = document.getElementById('send-btn');
-const cancelButton = document.getElementById ('cancel-btn');
-let prezzoBiglietto = document.getElementById('price');
+
+let kmPercorso = document.getElementById('km');
+let etaPasseggero = document.getElementById('age');
+let sendButton = document.getElementById('send-btn');
+let cancelButton = document.getElementById ('cancel-btn');
+let nomePass = document.getElementById ('nomePass')
+let costoTicket = document.getElementById('costoTicket')
+
+// let prezzoBiglietto = document.getElementById('price');
+
+//inizializzo boxBiglietto in una variabile
+
+let boxBiglietto = document.getElementById("boxBiglietto")
+
+//creo una funzione che gestisce il display
+
+function displayShow (){
+    
+    let nomePasseggero = document.querySelector('#name').value;
+    let kmPercorso = document.querySelector('#km').value;
+    let etaPasseggero = document.querySelector('#age').value;
+    let prezzo = 0.21 * kmPercorso;
+    let prezzoTotale = 0
+
+    if ( etaPasseggero <18 ) {
+        prezzoTotale = prezzo - prezzo * 0.20;
+        costoTicket.innerHTML = `${prezzoTotale.toFixed(2)}`
+    } else if ( etaPasseggero >=65 ) {
+        prezzoTotale = prezzo - prezzo * 0.40;
+        costoTicket.innerHTML = `${prezzoTotale.toFixed(2)}`
+    }   else {
+         
+            prezzoTotale = prezzo 
+        
+            costoTicket.innerHTML = `${prezzoTotale.toFixed(2)}`
+    }
+
+    
+    nomePass.innerHTML = `${nomePasseggero}`
+    // boxBiglietto.preventDefault();
+    boxBiglietto.classList.remove("box-none")
+    // boxBiglietto.classList.add("d-block")
+    
+    
+
+}
+
+
+function displayNone(){
+
+    boxBiglietto.classList.add("box-none")
+}
+
+//attribuisco la funzione ad un elemento per attivarla
